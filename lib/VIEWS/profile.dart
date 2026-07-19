1c511a7d54c1ab58a32cdd4bc4936e61d0787597
MARSDEN NGETICH <marsdengetich@gmail.com>
2026-07-19 10:35:55 +0300

    profile screen worked yess

--- lib/VIEWS/profile.dart
+++ lib/VIEWS/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_acs314b/configs/colors.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,6 +11,79 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Profile");
+    return Scaffold(
+      backgroundColor: thirdColor,
+      appBar: AppBar(
+        title: const Text("Profile"),
+        centerTitle: true,
+        backgroundColor: fifthColor,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Your Name : ",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: fourthColor,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.person_2),
+                prefixIconColor: sixthColor,
+              ),
+            ),
+
+            Text(
+              "Email: ",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: fourthColor,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email_outlined),
+                prefixIconColor: sixthColor,
+              ),
+            ),
+
+            Text(
+              "Phone : ",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: fourthColor,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.phone_android),
+                prefixIconColor: sixthColor,
+              ),
+            ),
+
+            SizedBox(height: 20),
+            Center(
+              child: MaterialButton(
+                onPressed: () {},
+                color: fifthColor,
+                height: 40,
+                minWidth: 120,
+                child: Text("Save", style: TextStyle(color: thirdColor)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/configs/colors.dart
+++ lib/configs/colors.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF00B9F1);
 
 const Color secondaryColor = Color(0xFF192F59);
+const Color thirdColor = Color(0xFFF5F5DC);
+const Color fifthColor = Color(0xFFFF8F00);
+const Color fourthColor = Color(0xFFFBC02D);
+const Color sixthColor = Color(0xFFC62828);
